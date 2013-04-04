2c1c7b80da25fd048b4a83e40787b36089539aa4
Travis Hinkelman <travis@datavoreconsulting.com>
2013-04-04 00:23:21 -0500

    Change name of column in data. Update model formula to reflect column name change.

--- server.R
+++ server.R
@@ -3,13 +3,15 @@ library(survival)
 
 myData = read.csv("PRTData.csv")
 myData$Aphid = relevel(myData$Aphid, ref = "pea")
-model = coxph(Surv(PRT,Censor) ~ Aphid*Starve, data = myData)
+model = coxph(Surv(PRT,NotCensored) ~ Aphid*Starve, data = myData)
 
 shinyServer(function(input, output) {
 
   output$plot_predicted <- renderPlot({ 
     nd = data.frame(Aphid=c("pea","bean"), Starve=input$starve)
+    # Plot mean survival curves
     plot(survfit(model,newdata=nd),col=c(2,1),lwd=2,conf.int=F,xlim=c(0,720), xlab="Time (min)",ylab="Proportion Remaining in Patch")
+    # Plot 95% confidence intervals
     lines(survfit(model,newdata=nd),conf.int=T,col=c(2,1),lty=2,lwd=1.5)
     legend(500,1,legend = c("Bean Aphid","Pea Aphid"),lwd=2,col=c(1,2),bty="n")  
   })
